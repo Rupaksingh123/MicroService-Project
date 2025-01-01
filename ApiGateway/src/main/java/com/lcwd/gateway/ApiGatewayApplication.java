48f8cc5e0fbbc75aeea3a98c6b3b937c4f96a2e7
Rupaksingh123 <rupaksingh12321@gmail.com>
2025-01-01 06:05:41 +0530

    Api Gateway

--- ApiGateway/src/main/java/com/lcwd/gateway/ApiGatewayApplication.java
+++ ApiGateway/src/main/java/com/lcwd/gateway/ApiGatewayApplication.java
@@ -3,7 +3,9 @@ package com.lcwd.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
